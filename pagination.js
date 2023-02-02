84b0fa844b3eff3b85c9e82153afb87b1f324e28
ssozr <a917382460@gmail.com>
2023-02-02 20:32:34 +0800

    更新3

--- pagination.js
+++ pagination.js
@@ -3,9 +3,10 @@ export default{
     template:`
     <nav aria-label="Page navigation example">
     <ul class="pagination">
-      <li class="page-item">
+      <li class="page-item"
+      :class="{ disabled : !pages.has_pre}">
         <a class="page-link" href="#" aria-label="Previous"
-        :class="{ disabled : !pages.has_pre}">
+      >
           <span aria-hidden="true">&laquo;</span>
         </a>
       </li>
@@ -17,9 +18,10 @@ export default{
         <a class="page-link" href="#">{{ page }}</a>
       </li>
       
-      <li class="page-item">
+      <li class="page-item"
+      :class="{ disabled : !pages.has_next}">
         <a class="page-link" href="#" aria-label="Next"
-        :class="{ disabled : !pages.has_next}">
+        >
           <span aria-hidden="true">&raquo;</span>
         </a>
       </li>
